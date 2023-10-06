28f8a1778e9ab549a6172ce3ab456f7380e6e1f6
Jorge Echeverria <coko.eche@gmail.com>
2023-10-06 15:27:48 +1100

    add view engines

--- Node-crash-course/server/app.js
+++ Node-crash-course/server/app.js
@@ -3,6 +3,9 @@ const express = require("express"); //it returns a function
 //express app
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 //listen to requests
 //it infers we want to use localhost so we do not need to add anything else
 //it also returns an instance of the server. we could store it in a constant
@@ -10,18 +13,32 @@ const app = express();
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("/views/index.html", { root: __dirname });
-});
-app.get("/about", (req, res) => {
-  res.sendFile("/views/about.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectur",
+    },
+    {
+      title: "How to defeat Bowser",
+      snippet: "Lorem ipsum dolor sit amet consectur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
-//redirect
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Blog" });
 });
 
 //404
 app.use((req, res) => {
-  res.status(404).sendFile("/views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
